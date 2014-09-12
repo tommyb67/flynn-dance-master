7e2e789ad1fa57d74653fee618579f12a5ea9ad7
Tom Brennan <tommybrennan@gmail.com>
2014-09-11 22:52:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlynnDanceMaster::Application.config.secret_key_base = '168fd348e98dec79da9d6c342fcd461948f211df53300815ecdd27bc07e772132ee935100d8175d6d1485dc25f55506dcec1a59ebc9f36b90676263ff18ea6fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlynnDanceMaster::Application.config.session_store :cookie_store, key: '_flynn_dance_master_session'
