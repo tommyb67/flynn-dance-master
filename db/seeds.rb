475a7bed6ba115e74f57c7850d776482dbeb92a3
Tom Brennan <tommybrennan@gmail.com>
2014-09-13 18:33:55 -0400

    began the seed of the feis table and working on the layout of the feis index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 
 Feis.create(date: "09/21/2014", name: "Syracuse Feis", location: "Drivers Village, 5885 East Circle Drive, Cicero, NY  13039", chairperson: "Gus Johnston", entry_secretary: "Johnston's Tax Service Inc", url: "www.johnstonstaxservice@yahoo.com");
+
+Feis.create(date: "09/21/2014", name: "Nassau County AOH Feis", location: "Hofstra University, 1000 Fulton Ave, Hempstead, NY  11550", chairperson: "Michael Byrne", entry_secretary: "Regina Begley", url: "www.nassauaohfeis.com");
+
+Feis.create(date: "09/28/2014", name: "Doherty Petri Feis", location: "Carle Place High School, 168 Cherry Lane, Carle Place, NY  11514", chairperson: "Carl Petri", entry_secretary: "Feisweb", url: "www.petrischool.com");
