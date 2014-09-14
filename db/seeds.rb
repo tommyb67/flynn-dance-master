4118fd6a3d975317df2c6d03ee58ae55a30fa1f3
Tom Brennan <tommybrennan@gmail.com>
2014-09-13 20:32:46 -0400

    additional seeds to the feis database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Feis.create(date: "09-21-2014", name: "Syracuse Feis", location: "Drivers Village, 5885 East Circle Drive, Cicero, NY  13039", chairperson: "Gus Johnston", entry_secretary: "Johnston's Tax Service Inc", url: "www.johnstonstaxservice@yahoo.com");
+Feis.create(date: "09-21-2014", name: "Syracuse Feis", location: "Drivers Village, 5885 East Circle Drive, Cicero, NY  13039", chairperson: "Gus Johnston", entry_secretary: "Johnston's Tax Service Inc", url: "www.johnstonirishdance.com");
 
 Feis.create(date: "09-21-2014", name: "Nassau County AOH Feis", location: "Hofstra University, 1000 Fulton Ave, Hempstead, NY  11550", chairperson: "Michael Byrne", entry_secretary: "Regina Begley", url: "www.nassauaohfeis.com");
 
 Feis.create(date: "09-28-2014", name: "Doherty Petri Feis", location: "Carle Place High School, 168 Cherry Lane, Carle Place, NY  11514", chairperson: "Carl Petri", entry_secretary: "Feisweb", url: "www.petrischool.com");
+
+Feis.create(date: "10-04-2014", name: "McDonald School Fall Feis", location: "J.M. McDonald Sports Complex, 4292 Fairgrounds Drive, Cortland, NY  13045", chairperson: "Amy Markwardt", entry_secretary: "Karen Frost", url: "www.cnyirishdance.com");
+
+Feis.create(date: "10-05-2014", name: "Young School Feis", location: "Main Street Armory, 900 East Main Street, Rochester, NY  14605", chairperson: "Sandy Oswald & Rachel Young", entry_secretary: "Peg Abbamonte", url: "www.youngschoolirishdance.com");
+
+Feis.create(date: "10-05-2014", name: "New Jersey Championship-McLoughlin School Feis", location: "Rockland Community College, College Road, Suffern, NY ", chairperson: "James Early", entry_secretary: "Elaine Greenan", url: "www.mcloughlinschool.com");
